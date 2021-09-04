5e5df5e6057adf2a65a8fc2cddad57ea922f374f
Je <190894@students.aih.nsw.edu.au>
2021-09-04 23:10:06 +1000

    Firebase connection and Registration writing information to firebase.

--- app/src/main/java/com/example/quickdel/RegistrationActivity2.java
+++ app/src/main/java/com/example/quickdel/RegistrationActivity2.java
@@ -8,14 +8,63 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class RegistrationActivity2 extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_registration2);
+
+        //Hooks
+        regName = findViewById(R.id.name);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regPhoneNo = findViewById(R.id.phone);
+        regPassword = findViewById(R.id.password);
+        regBtn = findViewById(R.id.btnSignUp);
+        regToLoginBtn = findViewById(R.id.btnSignIn);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
+
+                reference.child(phoneNo).setValue(helperClass);
+                Toast.makeText(RegistrationActivity2.this , "Successfully Registered!", Toast.LENGTH_LONG).show();
+            }
+
+
+
+        });
+
+
+
     }
 
 
