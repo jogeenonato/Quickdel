ce4199f57f7b255bccbc51249e7cad9ddacc8191
Jogee Nonato <68215914+jogeenonato@users.noreply.github.com>
2021-10-08 18:08:26 +1100

    Fixed the Payment Method for Runner

--- app/src/main/java/com/example/quickdel/RunnerPaymentMethod.java
+++ app/src/main/java/com/example/quickdel/RunnerPaymentMethod.java
@@ -3,20 +3,14 @@ package com.example.quickdel;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 public class RunnerPaymentMethod extends AppCompatActivity {
 
@@ -33,18 +27,11 @@ public class RunnerPaymentMethod extends AppCompatActivity {
         accName = findViewById(R.id.acc_name);
         accBSB = findViewById(R.id.acc_bsb);
         accNumber = findViewById(R.id.acc_number);
+        save = findViewById(R.id.savepayment);
 
-        runnerDBRef = FirebaseDatabase.getInstance().getReference().child("runners");
+        runnerDBRef = FirebaseDatabase.getInstance().getReference().child("RunnerBank");
 
-
-
-
-        save.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View view){
-               savePaymentDetails();
-;           }
-        });
+        save.setOnClickListener(view -> savePaymentDetails());
     }
 
     private void savePaymentDetails(){
@@ -52,24 +39,14 @@ public class RunnerPaymentMethod extends AppCompatActivity {
         String accountBSB = accBSB.getText().toString();
         String accountNo = accNumber.getText().toString();
 
+        SharedPreferences settings1 = getSharedPreferences("Runner", Context.MODE_PRIVATE);
+        String runnerUID = settings1.getString("runnerID", "");
 
         RunnerPaymentData runnerPaymentData = new RunnerPaymentData(accountName, accountBSB, accountNo);
 
-        SharedPreferences settings = getSharedPreferences("Profile", Context.MODE_PRIVATE);
-        String runner = settings.getString("name", "");
-        Query checkUser = runnerDBRef.orderByChild("userName").equalTo(runner);
-        checkUser.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot){
-                runnerDBRef.child("Payment").setValue(runnerPaymentData);
-                Toast.makeText(RunnerPaymentMethod.this,"Bank Details Successfully Saved", Toast.LENGTH_SHORT).show();
-            }
+        runnerDBRef.child(runnerUID).setValue(runnerPaymentData);
+        Toast.makeText(RunnerPaymentMethod.this,"Bank Details Successfully Saved", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
 }
