46cac566e39e7c0ec9a21ea63737d184dc2b8d7f
Je <190894@students.aih.nsw.edu.au>
2021-10-09 17:19:57 +1100

    Tracklist updated. payment fix

--- app/src/main/java/com/example/quickdel/RunnerPaymentMethod.java
+++ app/src/main/java/com/example/quickdel/RunnerPaymentMethod.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,7 +21,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class RunnerPaymentMethod extends AppCompatActivity {
-
+    ImageView back_btn;
     Button save;
     EditText accName, accBSB, accNumber;
     DatabaseReference runnerDBRef;
@@ -28,11 +30,15 @@ public class RunnerPaymentMethod extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_runner_payment_method);
+
+        back_btn = findViewById(R.id.btn_back);
 
         accName = findViewById(R.id.acc_name);
         accBSB = findViewById(R.id.acc_bsb);
         accNumber = findViewById(R.id.acc_number);
+        save = findViewById(R.id.savepayment);
 
         runnerDBRef = FirebaseDatabase.getInstance().getReference().child("runners");
 
@@ -44,6 +50,13 @@ public class RunnerPaymentMethod extends AppCompatActivity {
            public void onClick(View view){
                savePaymentDetails();
 ;           }
+        });
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RunnerPaymentMethod.super.onBackPressed();
+            }
         });
     }
 
@@ -71,5 +84,8 @@ public class RunnerPaymentMethod extends AppCompatActivity {
             }
         });
     }
-
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
 }
