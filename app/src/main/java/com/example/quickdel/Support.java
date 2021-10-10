3baddd19222c387a04639e8155691f2abf34351c
Francisco Forero <franciscoforero48@gmail.com>
2021-10-10 20:15:57 +1100

    change place an order, and earnings icon, changed all fonts for Prompt semibold italic

--- app/src/main/java/com/example/quickdel/Support.java
+++ app/src/main/java/com/example/quickdel/Support.java
@@ -1,31 +1,43 @@
 package com.example.quickdel;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Support extends AppCompatActivity {
-    private Button button;
+    Button button;
+    EditText mailtotext, subjecttext, compose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
 
-        button = (Button) findViewById(R.id.button7);
+        mailtotext = findViewById(R.id.mailtotext);
+        //subjecttext = findViewById(R.id.subjecttext);
+        //compose = findViewById(R.id.compose);
+       // button = (Button) findViewById(R.id.Send);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){ openMainActivity();
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW
+                , Uri.parse("mailto" + mailtotext.getText().toString()));
+                intent.putExtra(Intent.EXTRA_SUBJECT,subjecttext.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT,compose.getText().toString());
+
+                startActivity(intent);
             }
         });
 
 
     }
-    public void openMainActivity() {
-        Intent intent = new Intent(this, RunnerMain.class);
-        startActivity(intent);
-    }
+
+
 }
